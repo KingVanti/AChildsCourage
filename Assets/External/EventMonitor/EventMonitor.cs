32c0fdf0fe4738f299da58508f79f0054b1bd3f9
KingVanti <kingvanti650@gmail.com>
2020-10-12 14:22:27 +0200

    Add EventMonitor package

---
+++ Assets/External/EventMonitor/EventMonitor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace PADEAH.Game20483D.TestUtility
+{
+
+    public class EventMonitor<TEventArgs> where TEventArgs : EventArgs
+    {
+
+        #region Properties
+
+        public TEventArgs LastRaised { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public EventMonitor(object eventSource, string eventName)
+        {
+            SubscribeToEvent(eventSource, eventName);
+        }
+
+        public EventMonitor(object eventSource)
+        {
+            var eventArgsName = typeof(TEventArgs).Name;
+            var eventName = $"On{eventArgsName.Substring(0, eventArgsName.Length - 9)}";
+
+            SubscribeToEvent(eventSource, eventName);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void SubscribeToEvent(object eventSource, string eventName)
+        {
+            var sourceType = eventSource.GetType();
+            var eventInfo = sourceType.GetEvent(eventName);
+
+            if (eventInfo == null)
+                throw new ArgumentException($"No event with the name {eventName} found on type {sourceType.Name}", nameof(eventName));
+
+            var @delegate = Delegate.CreateDelegate(eventInfo.EventHandlerType, this, nameof(OnEvent), false, false);
+            eventInfo.AddEventHandler(eventSource, @delegate);
+        }
+
+        private void OnEvent(object sender, TEventArgs eventArgs)
+        {
+            LastRaised = eventArgs;
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Assets/External/EventMonitor/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PADEAH.Game20483D.TestUtility
+{
+
+    public static class Extensions
+    {
+
+        #region Methods
+
+        public static EventMonitor<TEventArgs> AttachMonitor<TEventArgs>(this object eventSource) where TEventArgs : EventArgs
+        {
+            return new EventMonitor<TEventArgs>(eventSource);
+        }
+
+        public static TEventArgs Capture<TEventArgs>(this object eventSource, Action action) where TEventArgs : EventArgs
+        {
+            var eventMonitor = eventSource.AttachMonitor<TEventArgs>();
+
+            action();
+
+            return eventMonitor.LastRaised;
+        }
+
+        #endregion
+
+    }
+
+}
