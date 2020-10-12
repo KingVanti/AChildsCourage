b4fa0eac93bdb96c1e93a02a001cb4beecc1076f
KingVanti <kingvanti650@gmail.com>
2020-10-12 12:43:52 +0200

    Add Ninject.Extensions.Unity library

---
+++ Packages/Ninject.Extensions.Unity/Internal/Utility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Ninject.Extensions.Unity
+{
+
+    internal static class Utility
+    {
+
+        #region Methods
+
+        internal static IEnumerable<MonoBehaviour> GetMonoBehavioursOfType(Type type)
+        {
+            return UnityEngine.Object.FindObjectsOfType<MonoBehaviour>().Where(m => m.GetType() == type);
+        }
+
+        #endregion
+
+    }
+
+}
