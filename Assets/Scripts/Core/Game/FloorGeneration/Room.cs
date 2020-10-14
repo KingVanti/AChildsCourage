9ad8ca8c80f8ced0f71d829c36ce4114b5d67190
KingVanti <kingvanti650@gmail.com>
2020-10-14 14:52:44 +0200

    Add data types for room shape storing

---
+++ Assets/Scripts/Core/Game/FloorGeneration/Room.cs
@@ -0,0 +1,17 @@
+﻿namespace AChildsCourage.Game.FloorGeneration
+{
+
+    public class Room
+    {
+
+        #region Properties
+
+        public string Name { get; }
+
+        public RoomShape Shape { get; }
+
+        #endregion
+
+    }
+
+}
