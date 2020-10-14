06df7943cce9656ded6f7fef59be68588c90dbac
KingVanti <kingvanti650@gmail.com>
2020-10-14 15:08:28 +0200

    Add room-asset loading logic

---
+++ Assets/Scripts/Core/Game/FloorGeneration/Persistance/_Interfaces/IRoomLoader.cs
@@ -0,0 +1,15 @@
+﻿namespace AChildsCourage.Game.FloorGeneration.Persistance
+{
+
+    public interface IRoomLoader
+    {
+
+        #region Methods
+
+        Room Load(int id);
+
+        #endregion
+
+    }
+
+}
