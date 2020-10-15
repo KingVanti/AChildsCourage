4c5c56a382171f5e477902f3dae416bca23585e9
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-10-15 10:38:26 +0200

    Add new temporary tilemaps

---
+++ Assets/Scripts/Presentation.Unity/Game/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AChildsCourage.Game.Input {
+    public class PlayerController : MonoBehaviour {
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+
+
+    }
+
+}
