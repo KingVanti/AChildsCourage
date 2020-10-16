9b86405265c47f38e2543fc23b4d7d0d63e66426
KingVanti <kingvanti650@gmail.com>
2020-10-16 11:55:44 +0200

    Add floor placing

---
+++ Assets/Scripts/Core/Game/Floors/Generation/TileType.cs
@@ -0,0 +1,9 @@
+﻿namespace AChildsCourage.Game.Floors.Generation
+{
+
+    public enum TileType
+    {
+        Floor
+    }
+
+}
---
+++ Assets/Scripts/Core/Game/Floors/Generation/_Interfaces/ITilePlacer.cs
@@ -0,0 +1,15 @@
+﻿namespace AChildsCourage.Game.Floors.Generation
+{
+
+    public interface ITilePlacer 
+    {
+
+        #region Methods
+
+        void Place(TileType type, TilePosition position);
+
+        #endregion
+
+    }
+
+}
