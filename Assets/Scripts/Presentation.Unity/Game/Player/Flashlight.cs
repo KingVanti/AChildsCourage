945e5b39b99776c4d0c6a4bc5d5a49bfa6a7f1d1
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-10-30 14:45:49 +0100

    Add Flashlight Script

---
+++ Assets/Scripts/Presentation.Unity/Game/Player/Flashlight.cs
@@ -0,0 +1,37 @@
+﻿using AChildsCourage.Game.Input;
+using Ninject.Extensions.Unity;
+using UnityEngine;
+
+namespace AChildsCourage.Game.Player {
+    public class Flashlight : MonoBehaviour {
+
+        [SerializeField] private Camera mainCamera;
+
+        [AutoInject]
+        public IInputListener InputListener {
+            set { BindTo(value); }
+        }
+
+        private Vector2 MousePos {
+            get; set;
+        }
+
+        private void FollowMousePosition() {
+
+            Vector2 projectedMousePos = mainCamera.ScreenToWorldPoint(MousePos);
+            transform.position = new Vector3(projectedMousePos.x,projectedMousePos.y,0);
+
+        }
+
+        private void BindTo(IInputListener listener) {
+            listener.OnMousePositionChanged += (_, e) => OnMousePositionChanged(e);
+        }
+
+        public void OnMousePositionChanged(MousePositionChangedEventArgs eventArgs) {
+            MousePos = eventArgs.MousePosition;
+            FollowMousePosition();
+        }
+
+
+    }
+}
