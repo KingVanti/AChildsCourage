51a7e8436c6e7d7a3eb61786819c94c17073aa37
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-10-30 14:46:12 +0100

    Drop unused code

--- Assets/Scripts/Presentation.Unity/Game/Player/GameCameraController.cs
+++ Assets/Scripts/Presentation.Unity/Game/Player/GameCameraController.cs
@@ -1,53 +1,8 @@
-﻿using AChildsCourage.Game.Input;
-using Ninject.Extensions.Unity;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace AChildsCourage.Game.Player {
     public class GameCameraController : MonoBehaviour {
-
-        #region Fields
-
-#pragma warning disable 649
-
-        [SerializeField] private CharacterController characterController;
-        [SerializeField] private Camera mainCamera;
-
-#pragma warning restore 649
-
-        private float cameraDistance = -10.0f;
-
-        #endregion
-
-        #region Properties
-
-        [AutoInject]
-        public IInputListener InputListener {
-            set { BindTo(value); }
-        }
-
-        public Vector3 MousePos {
-            get; private set;
-        }
-
-        #endregion
-
-        #region Methods
-
-        private void BindTo(IInputListener listener) {
-            listener.OnMousePositionChanged += (_, e) => OnMousePositionChanged(e);
-        }
-
-        private void LateUpdate() {
-            transform.position = new Vector3(characterController.transform.position.x, characterController.transform.position.y, cameraDistance);
-        }
-
-        public void OnMousePositionChanged(MousePositionChangedEventArgs eventArgs) {
-            MousePos = eventArgs.MousePosition;
-        }
-
-        #endregion
-
-
 
 
 
