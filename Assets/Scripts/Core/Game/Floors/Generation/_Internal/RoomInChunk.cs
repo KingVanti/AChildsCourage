b8789de56f76538ff6a52f617ad587c01a98f8c9
KingVanti <kingvanti650@gmail.com>
2020-11-03 13:57:36 +0100

    Add floor-generation data types

---
+++ Assets/Scripts/Core/Game/Floors/Generation/_Internal/RoomInChunk.cs
@@ -0,0 +1,27 @@
+﻿namespace AChildsCourage.Game.Floors.Generation
+{
+
+    public readonly struct RoomInChunk 
+    {
+        
+        #region Properties
+
+        public RoomInfo Room { get; }
+
+        public ChunkPosition Position { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public RoomInChunk(RoomInfo room, ChunkPosition position)
+        {
+            Room = room;
+            Position = position;
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Assets/Scripts/Core/Game/Floors/Generation/_Internal/RoomInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace AChildsCourage.Game.Floors.Generation
+{
+
+    public class RoomInfo
+    {
+
+        #region Properties
+
+        public int RoomId { get; }
+
+        public Passage[] Passages { get; }
+
+        #endregion
+
+    }
+
+}
