ee8d95b2e4d6113ccee5c0a3c87665b34f277dda
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-11-09 16:52:35 +0100

    Add Item script

---
+++ Assets/Scripts/Unity/Game/Player/Item.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace AChildsCourage.Game.Player {
+    public class Item : MonoBehaviour {
+
+
+        #region Methods
+        public void OnUse() {
+
+        }
+
+        #endregion
+
+
+
+
+    }
+
+}
