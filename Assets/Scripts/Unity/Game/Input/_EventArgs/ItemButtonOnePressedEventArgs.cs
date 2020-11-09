96c9c8cc4c14627fc516e9d17702f87908af7b8b
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-11-09 16:54:05 +0100

    Add Item buttons events

---
+++ Assets/Scripts/Unity/Game/Input/_EventArgs/ItemButtonOnePressedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AChildsCourage.Game.Input {
+    public class ItemButtonOnePressedEventArgs : EventArgs {
+
+        public bool IsPresssed { get; }
+
+        public ItemButtonOnePressedEventArgs(bool isPressed) {
+
+            IsPresssed = isPressed;
+
+        }
+    }
+}
--- Assets/Scripts/Unity/Game/Input/_internal/InputListener.cs
+++ Assets/Scripts/Unity/Game/Input/_internal/InputListener.cs
@@ -2,28 +2,28 @@
 using UnityEngine;
 using Context = UnityEngine.InputSystem.InputAction.CallbackContext;
 
-namespace AChildsCourage.Game.Input
-{
+namespace AChildsCourage.Game.Input {
 
     [Singleton]
-    internal class InputListener : IInputListener, IEagerActivation
-    {
+    internal class InputListener : IInputListener, IEagerActivation {
 
         public event EventHandler<MousePositionChangedEventArgs> OnMousePositionChanged;
         public event EventHandler<MoveDirectionChangedEventArgs> OnMoveDirectionChanged;
+        public event EventHandler<ItemButtonOnePressedEventArgs> OnItemButtonOnePressed;
+        public event EventHandler<ItemButtonTwoPressedEventArgs> OnItemButtonTwoPressed;
 
-        public InputListener()
-        {
+        public InputListener() {
 
             UserControls userControls = new UserControls();
             userControls.Player.Look.performed += OnLook;
             userControls.Player.Move.performed += OnMove;
+            userControls.Player.Item1.performed += OnItemOne;
+            userControls.Player.Item2.performed += OnItemTwo;
             userControls.Player.Enable();
 
         }
 
-        private void OnLook(Context context)
-        {
+        private void OnLook(Context context) {
 
             Vector2 mousePos = context.ReadValue<Vector2>();
             MousePositionChangedEventArgs eventArgs = new MousePositionChangedEventArgs(mousePos);
@@ -31,12 +31,27 @@ namespace AChildsCourage.Game.Input
 
         }
 
-        private void OnMove(Context context)
-        {
+        private void OnMove(Context context) {
 
             Vector2 moveDirection = context.ReadValue<Vector2>();
             MoveDirectionChangedEventArgs eventArgs = new MoveDirectionChangedEventArgs(moveDirection);
             OnMoveDirectionChanged?.Invoke(this, eventArgs);
+
+        }
+
+        private void OnItemOne(Context context) {
+
+            bool isPressed = context.ReadValueAsButton();
+            ItemButtonOnePressedEventArgs eventArgs = new ItemButtonOnePressedEventArgs(isPressed);
+            OnItemButtonOnePressed?.Invoke(this, eventArgs);
+
+        }
+
+        private void OnItemTwo(Context context) {
+
+            bool isPressed = context.ReadValueAsButton();
+            ItemButtonTwoPressedEventArgs eventArgs = new ItemButtonTwoPressedEventArgs(isPressed);
+            OnItemButtonTwoPressed?.Invoke(this, eventArgs);
 
         }
 
