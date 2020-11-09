1ff92026f73338b2962471d89ce849e9407f2b22
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-11-09 16:43:54 +0100

    Add EntityTag script

---
+++ Assets/Scripts/Unity/Game/EntityTags.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AChildsCourage.Game {
+
+    public static class EntityTags {
+
+        /// <summary>
+        /// Unity tag for enemy entities
+        /// </summary>
+        public const string Enemy = "Enemy";
+
+        /// <summary>
+        /// Unity tag for item entities
+        /// </summary>
+        public const string Item = "Item";
+
+        /// <summary>
+        /// Unity tag for player entities
+        /// </summary>
+        public const string Player = "Player";
+
+
+    }
+
+}
