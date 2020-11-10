2ba60569603da3aded620f62b47f6094e0ef9c0e
KingVanti <kingvanti650@gmail.com>
2020-11-10 16:13:01 +0100

    Add gui logic for loading room assets

---
+++ Assets/Scripts/Unity.RoomEditor/Editor/RoomEditorEditor.cs
@@ -0,0 +1,51 @@
+﻿using AChildsCourage.Game.Floors.Persistance;
+using UnityEditor;
+using UnityEngine;
+
+namespace AChildsCourage.RoomEditor.Editor
+{
+
+    [CustomEditor(typeof(RoomEditor))]
+    public class RoomEditorEditor : UnityEditor.Editor
+    {
+
+        #region Properties
+
+        private RoomEditor RoomEditor { get { return target as RoomEditor; } }
+
+        #endregion
+
+        #region Methods
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (Application.isPlaying)
+                DrawRoomEdiorGUI();
+            else
+                EditorGUILayout.LabelField("Press play to start editing!");
+        }
+
+        private void DrawRoomEdiorGUI()
+        {
+            if (!RoomEditor.HasLoadedAsset)
+                DrawLoadAssetGUI();
+        }
+
+        private void DrawLoadAssetGUI()
+        {
+            EditorGUILayout.LabelField("Drop in a room-asset to load it");
+            var selectedAsset = (RoomAsset)EditorGUILayout.ObjectField(null, typeof(RoomAsset), false);
+
+            EditorGUILayout.Space();
+
+            if (selectedAsset != null)
+                RoomEditor.OnAssetSelected(selectedAsset);
+        }
+
+        #endregion
+
+    }
+
+}
--- Assets/Scripts/Unity.RoomEditor/RoomEditor.cs
+++ Assets/Scripts/Unity.RoomEditor/RoomEditor.cs
@@ -1,4 +1,5 @@
 ﻿using AChildsCourage.Game.Floors.Persistance;
+using ICSharpCode.NRefactory.Visitors;
 using System;
 using UnityEngine;
 
@@ -7,6 +8,34 @@ namespace AChildsCourage.RoomEditor
 
     public class RoomEditor : MonoBehaviour
     {
+
+        #region Fields
+
+        private RoomAsset loadedAsset;
+
+        #endregion
+
+        #region Properties
+
+        public bool HasLoadedAsset { get { return loadedAsset != null; } }
+
+        #endregion
+
+        #region Methods
+
+        public void OnAssetSelected(RoomAsset asset)
+        {
+            loadedAsset = asset;
+
+            LoadAsset(asset);
+        }
+
+        private void LoadAsset(RoomAsset asset)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
 
     }
 
