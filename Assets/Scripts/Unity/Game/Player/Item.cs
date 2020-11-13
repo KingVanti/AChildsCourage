5bbf1cedddd13a34a7832911341cdae92710cf0e
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-11-13 16:42:48 +0100

    Refactor item.cs

--- Assets/Scripts/Unity/Game/Player/Item.cs
+++ Assets/Scripts/Unity/Game/Player/Item.cs
@@ -3,9 +3,15 @@
 namespace AChildsCourage.Game.Player {
     public abstract class Item : MonoBehaviour {
 
+        #region Fields
+
         [Range(0, 120)]
         [SerializeField] private float _cooldown;
         [SerializeField] private int _id;
+
+        #endregion
+
+        #region Properties
 
         public int Id {
             get; private set;
@@ -15,9 +21,16 @@ namespace AChildsCourage.Game.Player {
             get { return _cooldown; }
             set { _cooldown = value; }
         }
+
         public bool IsInBag { get; set; }
 
+        #endregion
+
+        #region Methods
+
         public abstract void Toggle();
+
+        #endregion
 
     }
 
