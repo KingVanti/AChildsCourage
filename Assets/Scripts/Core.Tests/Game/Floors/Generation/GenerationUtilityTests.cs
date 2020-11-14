e3638582396ab62ad9bc85cac537c1866b87b751
KingVanti <kingvanti650@gmail.com>
2020-11-14 10:10:51 +0100

    Drop unused code

--- Assets/Scripts/Core.Tests/Game/Floors/Generation/GenerationUtilityTests.cs
+++ Assets/Scripts/Core.Tests/Game/Floors/Generation/GenerationUtilityTests.cs
@@ -11,37 +11,6 @@ namespace AChildsCourage.Game.Floors.Generation
 
         #region Tests
 
-        [TestCase(Passage.North, Passage.North, false)]
-        [TestCase(Passage.North, Passage.East, false)]
-        [TestCase(Passage.North, Passage.South, true)]
-        [TestCase(Passage.North, Passage.West, false)]
-
-        [TestCase(Passage.East, Passage.North, false)]
-        [TestCase(Passage.East, Passage.East, false)]
-        [TestCase(Passage.East, Passage.South, false)]
-        [TestCase(Passage.East, Passage.West, true)]
-
-        [TestCase(Passage.South, Passage.North, true)]
-        [TestCase(Passage.South, Passage.East, false)]
-        [TestCase(Passage.South, Passage.South, false)]
-        [TestCase(Passage.South, Passage.West, false)]
-
-        [TestCase(Passage.West, Passage.North, false)]
-        [TestCase(Passage.West, Passage.East, true)]
-        [TestCase(Passage.West, Passage.South, false)]
-        [TestCase(Passage.West, Passage.West, false)]
-        public void Directions_Connect_Correctly(Passage first, Passage second, bool excpected)
-        {
-            // When
-
-            var actual = Generation.CanConnect(first, second);
-
-            // Then
-
-            Assert.That(actual, Is.EqualTo(excpected));
-        }
-    
-
         [Test]
         public void Get_Correct_Surrounding_Positions()
         {
--- Assets/Scripts/Core/Game/Floors/Generation/_Internal/Generation.cs
+++ Assets/Scripts/Core/Game/Floors/Generation/_Internal/Generation.cs
@@ -6,29 +6,7 @@ namespace AChildsCourage.Game.Floors.Generation
     internal static class Generation
     {
 
-        #region Fields
-
-        private static PossibleConnection[] possibleConnections = new[]
-        {
-            new PossibleConnection(Passage.North, Passage.South),
-            new PossibleConnection(Passage.East, Passage.West),
-            new PossibleConnection(Passage.South, Passage.North),
-            new PossibleConnection(Passage.West, Passage.East)
-        };
-
-        #endregion
-
         #region Methods
-
-        internal static bool CanConnect(Passage p1, Passage p2)
-        {
-            foreach (var connection in possibleConnections)
-                if (p1 == connection.First && p2 == connection.Second)
-                    return true;
-
-            return false;
-        }
-
 
         internal static IEnumerable<ChunkPosition> GetSurroundingPositions(ChunkPosition position)
         {
