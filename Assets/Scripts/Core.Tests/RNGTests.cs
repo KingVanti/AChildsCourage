ce3dfebc85104359039a2f8f45a196545acbcb0e
KingVanti <kingvanti650@gmail.com>
2020-11-14 10:01:15 +0100

    Refactor to increase readability

--- Assets/Scripts/Core.Tests/RNGTests.cs
+++ Assets/Scripts/Core.Tests/RNGTests.cs
@@ -1,5 +1,4 @@
 ﻿using NUnit.Framework;
-using System;
 
 namespace AChildsCourage
 {
@@ -11,30 +10,27 @@ namespace AChildsCourage
         #region Tests
 
         [Test]
-        public void Given_Same_Seed_The_RNG_Outcome_Is_Exactly_The_Same()
+        public void Two_RNGs_With_Same_Seed_Yield_Same_Values()
         {
-
             // Given
 
-            int seed = 31482823;
-            Random random = new Random(seed);
-            RNG rng1 = new RNG(seed);
-            RNG rng2 = new RNG(seed);
+            var seed = 31482823;
+            var rng1 = new RNG(seed);
+            var rng2 = new RNG(seed);
 
-            // When
+            // When/Then
 
-            float rng3 = rng1.GetValueBetween(4.123f, 8.6622f);
-            float rng4 = rng2.GetValueBetween(4.123f, 8.6622f);
+            for (var i = 0; i < 10; i++)
+            {
+                var value1 = rng1.GetValueBetween(4.123f, 8.6622f);
+                var value2 = rng2.GetValueBetween(4.123f, 8.6622f);
 
-            // Then
-
-            Assert.That(rng3 == rng4);
-
+                Assert.That(value1, Is.EqualTo(value2), $"Values differed after {i} queries!");
+            }
         }
 
 
         #endregion
-
 
     }
 
