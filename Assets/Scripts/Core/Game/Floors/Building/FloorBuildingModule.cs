43c75e1d56f8033381000825a2032dda8d71a59f
KingVanti <kingvanti650@gmail.com>
2020-11-15 09:33:32 +0100

    Reformat files

--- Assets/Scripts/Core/Game/Floors/Building/FloorBuildingModule.cs
+++ Assets/Scripts/Core/Game/Floors/Building/FloorBuildingModule.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using static AChildsCourage.Game.Floors.Generation.GenerationModule;
--- Assets/Scripts/Core/Game/NightLoader.cs
+++ Assets/Scripts/Core/Game/NightLoader.cs
@@ -1,5 +1,4 @@
-﻿using AChildsCourage.Game.Floors;
-using AChildsCourage.Game.Floors.Building;
+﻿using AChildsCourage.Game.Floors.Building;
 using AChildsCourage.Game.Floors.Generation;
 using AChildsCourage.Game.Persistance;
 
