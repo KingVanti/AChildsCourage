97edaf29da48f1df757ec9b9487b1d507a313b8a
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-11-17 18:38:30 +0100

    Fix CourageBar

--- Assets/Scripts/Unity/Game/Courage/_Public/CouragePickup.cs
+++ Assets/Scripts/Unity/Game/Courage/_Public/CouragePickup.cs
@@ -19,6 +19,9 @@ namespace AChildsCourage.Game.Courage {
 
         #region Methods
 
+        /// <summary>
+        /// REMOVE LATER
+        /// </summary>
         private void Start() {
             SetCourageData(testPickup);
         }
