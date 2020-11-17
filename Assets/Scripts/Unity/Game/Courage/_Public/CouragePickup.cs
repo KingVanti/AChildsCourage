441edd5e06d83c0abc9f745a39b1c47d45af3c9b
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-11-17 17:45:18 +0100

    Add CouragePickupData Script

---
+++ Assets/Scripts/Unity/Game/Courage/_Public/CouragePickup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+namespace AChildsCourage.Game.Courage {
+    public class CouragePickup : MonoBehaviour {
+
+        #region Fields
+
+#pragma warning disable 649
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private CouragePickupData testPickup;
+#pragma warning restore 649
+
+        private int value;
+        private int id;
+
+        #endregion
+
+        #region Methods
+
+        private void Start() {
+            SetCourageData(testPickup);
+        }
+
+        public void SetCourageData(CouragePickupData courageData) {
+
+            id = courageData.Id;
+            value = courageData.Value;
+            spriteRenderer.sprite = courageData.Sprite;
+            spriteRenderer.transform.localScale = courageData.Scale;
+
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Assets/Scripts/Unity/Game/Courage/_Public/CouragePickupData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+namespace AChildsCourage.Game.Courage {
+
+    [CreateAssetMenu(fileName = "Courage", menuName = "A Child's Courage/Courage", order = 3)]
+    public class CouragePickupData : ScriptableObject {
+
+        #region Fields
+
+#pragma warning disable 649
+        [SerializeField] private int _value;
+        [SerializeField] private int _id;
+        [SerializeField] private Sprite _sprite;
+        [SerializeField] private Vector3 _scale;
+#pragma warning restore 649
+
+        #endregion
+
+        #region Properties
+
+        public int Id { get { return _id; } }
+
+        public int Value { get { return _value; } }
+
+        public Sprite Sprite { get { return _sprite; } }
+
+        public Vector3 Scale { get { return _scale; } }
+
+        #endregion
+
+
+
+    }
+}
