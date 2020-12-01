69bb3a67acee1a12fad3f563e3cabeb6b6aa87a3
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-12-01 15:47:00 +0100

    Add Investigation Coroutine

--- Assets/Scripts/Unity/Game/Monsters/Shade.cs
+++ Assets/Scripts/Unity/Game/Monsters/Shade.cs
@@ -24,13 +24,18 @@ namespace AChildsCourage.Game.Monsters
         private IEnumerable<TilePosition> currentTilesInVision;
         private InvestigationHistory investigationHistory = Empty;
 
+        private Coroutine investigationCoroutine;
+
 
         private MonsterState CurrentState => new MonsterState(Position, DateTime.Now, investigationHistory);
 
-        private EntityPosition Position => throw new NotImplementedException();
+        private EntityPosition Position => new EntityPosition(transform.position.x, transform.position.y);
 
         private FloorState FloorState => floorStateKeeper.CurrentFloorState;
 
+        private void Awake() {
+            Invoke(nameof(StartInvestigation), 1f);
+        }
 
         public void OnTilesInVisionChanged(IEnumerable<TilePosition> positions)
         {
@@ -50,17 +55,19 @@ namespace AChildsCourage.Game.Monsters
 
         private void StartInvestigation()
         {
-            // TODO: Start coroutine
+            investigationCoroutine = StartCoroutine(Investigate());
         }
 
         private void CancelInvestigation()
         {
-            // TODO: Stop coroutine
+            StopCoroutine(investigationCoroutine);
+            investigationCoroutine = null;
         }
 
         private IEnumerator Investigate()
         {
             var investigation = StartNew(FloorState, CurrentState, RNG.New());
+
             var currentTarget = NextTarget(investigation, Position);
             SetPathFinderTarget(currentTarget);
 
@@ -69,8 +76,10 @@ namespace AChildsCourage.Game.Monsters
                 investigation = Progress(investigation, currentTilesInVision);
 
                 var newTarget = NextTarget(investigation, Position);
-                if (!newTarget.Equals(currentTarget))
+                if (!newTarget.Equals(currentTarget)) {
                     SetPathFinderTarget(newTarget);
+                    currentTarget = newTarget;
+                }
 
                 yield return new WaitForSeconds(1f / investigationUpdatesPerSecond);
             }
@@ -83,7 +92,10 @@ namespace AChildsCourage.Game.Monsters
 
         private void SetPathFinderTarget(TilePosition tilePosition)
         {
-            throw new NotImplementedException();
+            Vector3 newPos = tilePosition.ToVector3() + new Vector3(0.5f, 0.5f, 0);
+            Vector3 deltaPos = newPos - transform.position;
+            transform.GetChild(0).right = deltaPos;
+            transform.position = newPos;
         }
 
     }
