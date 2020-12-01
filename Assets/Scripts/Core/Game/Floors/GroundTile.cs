ad73909d52fd0f69fe502379836c49bf091cf836
KingVanti <kingvanti650@gmail.com>
2020-12-01 15:03:57 +0100

    Add logic for generating floor state

--- Assets/Scripts/Core/Game/Floors/GroundTile.cs
+++ Assets/Scripts/Core/Game/Floors/GroundTile.cs
@@ -1,4 +1,5 @@
-﻿using static AChildsCourage.Game.MTilePosition;
+﻿using AChildsCourage.Game.Monsters.Navigation;
+using static AChildsCourage.Game.MTilePosition;
 
 namespace AChildsCourage.Game.Floors
 {
@@ -8,15 +9,18 @@ namespace AChildsCourage.Game.Floors
 
         #region Properties
 
+        public AOIIndex AOIIndex { get; }
+
         public TilePosition Position { get; }
 
         #endregion
 
         #region Constructors
 
-        public GroundTile(TilePosition position)
+        public GroundTile(AOIIndex aoiIndex, TilePosition position)
         {
             Position = position;
+            AOIIndex = aoiIndex;
         }
 
         #endregion
