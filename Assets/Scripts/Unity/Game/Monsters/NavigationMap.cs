9cf57a6832c1ef8bc96c2656b6fe803ad0f5df30
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-12-02 13:24:01 +0100

    Add Shade Pathfindng

---
+++ Assets/Scripts/Unity/Game/Monsters/NavigationMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+
+namespace AChildsCourage.Game.Monsters {
+    public class NavigationMap : MonoBehaviour {
+
+        [SerializeField] private AstarPath astarPath;
+        private NavGraph[] navigationGraph;
+
+        public void ScanLevel() {
+            navigationGraph = astarPath.graphs;
+            Debug.Log(navigationGraph[0]);
+            astarPath.Scan(navigationGraph[0]);
+        }
+
+    }
+}
