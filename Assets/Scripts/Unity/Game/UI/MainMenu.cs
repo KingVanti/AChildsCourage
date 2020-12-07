3c8381341e1e3b73187039050ad27795b66aa35f
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-12-07 10:39:36 +0100

    Add MainMenu Scene

---
+++ Assets/Scripts/Unity/Game/UI/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AChildsCourage.Game.UI {
+    public class MainMenu : MonoBehaviour {
+
+        public void OnPlayButtonPressed() {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(SceneNames.GameScene);
+        }
+
+    }
+}
--- Assets/Scripts/Unity/SceneNames.cs
+++ Assets/Scripts/Unity/SceneNames.cs
@@ -1,7 +1,7 @@
 ﻿internal static class SceneNames
 {
 
-    public const string GameOverScene = "GameOver";
+    public const string EndScene = "EndScene";
     public const string GameScene = "Game";
     public const string MainMenuScene = "MainMenu";
 
