096ef7af0149bc376c0c4fa107b8a222a6aa6933
KingVanti <kingvanti650@gmail.com>
2020-12-07 15:45:31 +0100

    Add static-object spawning

---
+++ Assets/Scripts/Unity/Game/Floors/StaticObjectEntity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace AChildsCourage.Game.Floors
+{
+
+    public class StaticObjectEntity : MonoBehaviour
+    {
+
+
+#pragma warning disable 649
+
+        [SerializeField] private SpriteRenderer spriteRenderer;
+
+#pragma warning  restore 649
+
+        public StaticObjectAppearance Appearance { set => spriteRenderer.sprite = value.Sprite; }
+
+    }
+
+}
