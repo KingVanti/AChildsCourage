2d96e18d6f762728c6094c8d83e5e04451417bf2
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-12-09 18:49:31 +0100

    Add Veins Script

---
+++ Assets/Scripts/Unity/Game/UI/Veins.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AChildsCourage.Game.UI {
+    public class Veins : MonoBehaviour {
+
+        [SerializeField] private Image image;
+
+        public void SetTransparency(float awareness) {
+            image.color = new Color(image.color.r, image.color.g, image.color.b,awareness);
+        }
+
+    }
+
+}
