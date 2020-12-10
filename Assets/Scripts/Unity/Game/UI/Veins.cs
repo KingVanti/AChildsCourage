9dfb1f372febad44aa493d658965024bb13e96a5
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2020-12-10 11:34:06 +0100

    Add Veins when shade is hunting

--- Assets/Scripts/Unity/Game/UI/Veins.cs
+++ Assets/Scripts/Unity/Game/UI/Veins.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using AChildsCourage.Game.Shade;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,9 +8,21 @@ namespace AChildsCourage.Game.UI {
     public class Veins : MonoBehaviour {
 
         [SerializeField] private Image image;
+        [SerializeField] private Sprite defaultVeins;
+        [SerializeField] private Sprite activeVeins;
 
         public void SetTransparency(float awareness) {
             image.color = new Color(image.color.r, image.color.g, image.color.b,awareness);
+        }
+
+        public void SetVeinSprite(AwarenessLevel level) {
+
+            if(level == AwarenessLevel.Hunting) {
+                image.sprite = activeVeins;
+            } else {
+                image.sprite = defaultVeins;
+            }
+
         }
 
     }
