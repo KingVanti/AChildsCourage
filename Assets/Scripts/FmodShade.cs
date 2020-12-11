397d254e3b9b9f8174a2aed68e50cfe1d33878bd
Sev <richter.severin@a1.net>
2020-12-11 15:45:31 +0100

    add shade banish sound

--- Assets/Scripts/FmodShade.cs
+++ Assets/Scripts/FmodShade.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using FMODUnity;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,13 @@ public class FmodShade : MonoBehaviour
 {
     FMODUnity.StudioEventEmitter emitter;
     [SerializeField] public GameObject player;
+
+    #region eventpaths
+    private const string ShadeBanished_path = "event:/Monster/getBanished";
+
+
+    #endregion
+
     float distance;
     private void OnEnable()
     {
@@ -18,5 +26,11 @@ public class FmodShade : MonoBehaviour
         distance = Mathf.Abs(Vector2.Distance(transform.position, player.transform.position));
         emitter.SetParameter("Distance", distance);
     }
+
+    public void Shade_getBanished()
+    {
+        RuntimeManager.PlayOneShot(ShadeBanished_path, GetComponent<Transform>().position);
+    }
+
 }
 
