53ec3219ba45371dc451aa99c058957b2d04c7df
KingVanti <kingvanti650@gmail.com>
2020-12-11 11:55:37 +0100

    Add logic for making runes inactive

--- Assets/Scripts/Unity/Game/Floors/RuneEntity.cs
+++ Assets/Scripts/Unity/Game/Floors/RuneEntity.cs
@@ -7,9 +7,21 @@ namespace AChildsCourage.Game.Floors
     public class RuneEntity : MonoBehaviour
     {
 
+        private bool isActive = true;
+
+
         public void OnTriggerEnter2D(Collider2D other)
         {
+            if (!isActive)
+                return;
             BanishShade(other);
+            Deactivate();
+        }
+
+        private void Deactivate()
+        {
+            spriteRenderer.sprite = inactiveSprite;
+            isActive = false;
         }
 
         private static void BanishShade(Collider2D other)
@@ -17,6 +29,13 @@ namespace AChildsCourage.Game.Floors
             var shadeBrain = other.GetComponent<ShadeBrain>();
             shadeBrain.Banish();
         }
+
+#pragma warning  disable 649
+
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private Sprite inactiveSprite;
+
+#pragma warning  restore 649
 
     }
 
