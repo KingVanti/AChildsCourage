90926ad53d1b409cbe651c1fe3be35ac2d9b5349
KingVanti <kingvanti650@gmail.com>
2020-12-16 14:44:19 +0100

    Drop regions

--- Assets/Scripts/Core/Game/Floors/GroundTile.cs
+++ Assets/Scripts/Core/Game/Floors/GroundTile.cs
@@ -6,24 +6,17 @@ namespace AChildsCourage.Game.Floors
 
     public readonly struct GroundTile
     {
-
-        #region Properties
-
+        
         public AoiIndex AoiIndex { get; }
 
         public TilePosition Position { get; }
 
-        #endregion
-
-        #region Constructors
 
         public GroundTile(AoiIndex aoiIndex, TilePosition position)
         {
             Position = position;
             AoiIndex = aoiIndex;
         }
-
-        #endregion
 
     }
 
