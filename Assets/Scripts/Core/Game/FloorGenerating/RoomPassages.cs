8d69e1becf61c5fe8ea0277afeb82b54cd737024
KingVanti <kingvanti650@gmail.com>
2020-12-16 14:54:53 +0100

    Refactor code into module

---
+++ Assets/Scripts/Core/Game/FloorGenerating/RoomPassages.cs
@@ -0,0 +1,38 @@
+﻿using AChildsCourage.Game.Floors;
+using AChildsCourage.Game.Floors.RoomPersistence;
+using static AChildsCourage.Game.Floors.MChunkPassages;
+
+namespace AChildsCourage.Game
+{
+
+    public static partial class MFloorGenerating
+    {
+
+        public class RoomPassages
+        {
+
+            public RoomId Id { get; }
+
+            public ChunkPassages Passages { get; }
+
+            public int RotationCount { get; }
+
+            public bool IsMirrored { get; }
+
+            public RoomType Type { get; }
+
+
+            public RoomPassages(RoomId id, ChunkPassages passages, int rotationCount = 0, bool isMirrored = false, RoomType type = RoomType.Normal)
+            {
+                Id = id;
+                Passages = passages;
+                Type = type;
+                RotationCount = rotationCount;
+                IsMirrored = isMirrored;
+            }
+
+        }
+
+    }
+
+}
