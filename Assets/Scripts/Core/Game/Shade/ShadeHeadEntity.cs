03ce10edb7b2571667b4adefd04e43858323677c
KingVanti <kingvanti650@gmail.com>
2020-12-19 15:36:48 +0100

    Reformat ShadeHeadEntity

--- Assets/Scripts/Core/Game/Shade/ShadeHeadEntity.cs
+++ Assets/Scripts/Core/Game/Shade/ShadeHeadEntity.cs
@@ -6,22 +6,13 @@ namespace AChildsCourage.Game.Shade
 
     public class ShadeHeadEntity : MonoBehaviour
     {
-
-        #region Fields
-
-        [SerializeField] private float rotationDegreesPerSecond;
-
+        
         [FindInScene] private ShadeMovementEntity shadeMovement;
 
-        #endregion
-
-        #region Methods
 
         private void Update() => FaceMovementDirection();
 
         private void FaceMovementDirection() => transform.right = shadeMovement.CurrentDirection;
-
-        #endregion
 
     }
 
