564ff5551e8753b35c31364545e7a29db9663ca7
KingVanti <kingvanti650@gmail.com>
2020-12-29 15:55:15 +0100

    Refactor to increase readability

--- Assets/Scripts/Core/Game/TilePosition.cs
+++ Assets/Scripts/Core/Game/TilePosition.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using static UnityEngine.Mathf;
@@ -10,51 +9,40 @@ namespace AChildsCourage.Game
     public static class MTilePosition
     {
 
-        internal static Func<TilePosition, float> GetDistanceFromOrigin =>
-            position =>
-                new Vector2(position.X, position.Y).magnitude;
+        private const float TileSize = 1f;
+        private const float TileExtent = TileSize / 2f;
 
 
-        internal static Func<TilePosition, TilePosition, float> DistanceTo =>
-            (p1, p2) =>
-                Vector2.Distance(new Vector2(p1.X, p1.Y), new Vector2(p2.X, p2.Y));
+        internal static float GetDistanceFromOrigin(TilePosition position) =>
+            new Vector2(position.X, position.Y).magnitude;
+        
+        internal static float DistanceTo(TilePosition p1, TilePosition p2) =>
+            Vector2.Distance(new Vector2(p1.X, p1.Y), new Vector2(p2.X, p2.Y));
 
-        public static Func<TilePosition, TileOffset, TilePosition> OffsetBy =>
-            (position, offset) =>
-                new TilePosition(position.X + offset.X,
-                                 position.Y + offset.Y);
+        public static TilePosition OffsetBy(TilePosition position, TileOffset offset) =>
+            new TilePosition(position.X + offset.X,
+                             position.Y + offset.Y);
 
-        public static Func<TilePosition, Vector3Int> ToVector3Int =>
-            tilePosition =>
-                new Vector3Int(tilePosition.X,
-                               tilePosition.Y,
-                               0);
+        public static Vector3Int ToVector3Int(TilePosition position) =>
+            new Vector3Int(position.X, position.Y, 0);
 
-        public static Func<TilePosition, Vector2> ToVector2 =>
-            tilePosition =>
-                new Vector2(tilePosition.X,
-                            tilePosition.Y);
+        public static Vector2 ToVector2(TilePosition position) =>
+            new Vector2(position.X, position.Y);
 
-        public static Func<TilePosition, Vector2> GetTileCenter =>
-            tilePosition =>
-                new Vector2(tilePosition.X + 0.5f,
-                            tilePosition.Y + 0.5f);
+        public static Vector2 GetTileCenter(TilePosition position) =>
+            new Vector2(position.X + TileExtent, position.Y + TileExtent);
 
-        public static Func<Vector2, TilePosition> ToTile =>
-            vector =>
-                new TilePosition(FloorToInt(vector.x),
-                                 FloorToInt(vector.y));
+        public static TilePosition ToTile(Vector2 vector) =>
+            new TilePosition(FloorToInt(vector.x),
+                             FloorToInt(vector.y));
 
-        public static Func<TilePosition, float, IEnumerable<TilePosition>> FindPositionsInRadius =>
-            (center, radius) =>
-                GeneratePositionsInRadius(GetTileCenter(center), radius)
-                    .Select(ToTile);
-
-
-        private static Func<Vector2, float, IEnumerable<Vector2>> GeneratePositionsInRadius =>
-            (center, radius) =>
-                GeneratePositionsInSquare(center, radius)
-                    .Where(v => Vector2.Distance(v, center) <= radius);
+        public static IEnumerable<TilePosition> FindPositionsInRadius(TilePosition center, float radius) =>
+            GeneratePositionsInRadius(GetTileCenter(center), radius)
+                .Select(ToTile);
+        
+        private static IEnumerable<Vector2> GeneratePositionsInRadius(Vector2 center, float radius) =>
+            GeneratePositionsInSquare(center, radius)
+                .Where(v => Vector2.Distance(v, center) <= radius);
 
         private static IEnumerable<Vector2> GeneratePositionsInSquare(Vector2 center, float extend)
         {
@@ -62,6 +50,7 @@ namespace AChildsCourage.Game
                 for (var dY = -extend; dY <= extend; dY++)
                     yield return new Vector2(center.x + dX, center.y + dY);
         }
+
 
         public readonly struct TilePosition
         {
