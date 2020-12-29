6b1c93bc231116cfed7b558eedcda6c66d799c73
KingVanti <kingvanti650@gmail.com>
2020-12-29 15:46:47 +0100

    Reformat file

--- Assets/Scripts/Core/Functional.cs
+++ Assets/Scripts/Core/Functional.cs
@@ -9,7 +9,8 @@ namespace AChildsCourage
     public static class F
     {
 
-        public static TItem Take<TItem>(TItem input) => input;
+        public static TItem Take<TItem>(TItem input) =>
+            input;
 
         public static TItem For<TItem>(this TItem input, int times, Func<TItem, TItem> function)
         {
@@ -43,22 +44,28 @@ namespace AChildsCourage
             return result;
         }
 
-        public static TItem DoIf<TItem>(this TItem input, Func<TItem, TItem> function, bool predicate) => predicate ? function(input) : input;
+        public static TItem DoIf<TItem>(this TItem input, Func<TItem, TItem> function, bool predicate) =>
+            predicate ? function(input) : input;
 
         public static void While(this Action action, Func<bool> predicate)
         {
             while (predicate()) action();
         }
 
-        public static TResult Map<TItem, TResult>(this TItem item, Func<TItem, TResult> function) => function(item);
+        public static TResult Map<TItem, TResult>(this TItem item, Func<TItem, TResult> function) =>
+            function(item);
 
-        public static TResult Map<TItem, TResult, TP1>(this TItem item, Func<TItem, TP1, TResult> function, TP1 p1) => function(item, p1);
+        public static TResult Map<TItem, TResult, TP1>(this TItem item, Func<TItem, TP1, TResult> function, TP1 p1) =>
+            function(item, p1);
 
-        public static TResult Map<TItem, TResult, TP1, TP2>(this TItem item, Func<TItem, TP1, TP2, TResult> function, TP1 p1, TP2 p2) => function(item, p1, p2);
+        public static TResult Map<TItem, TResult, TP1, TP2>(this TItem item, Func<TItem, TP1, TP2, TResult> function, TP1 p1, TP2 p2) =>
+            function(item, p1, p2);
 
-        public static TResult Map<TItem, TResult, TP1, TP2, TP3>(this TItem item, Func<TItem, TP1, TP2, TP3, TResult> function, TP1 p1, TP2 p2, TP3 p3) => function(item, p1, p2, p3);
+        public static TResult Map<TItem, TResult, TP1, TP2, TP3>(this TItem item, Func<TItem, TP1, TP2, TP3, TResult> function, TP1 p1, TP2 p2, TP3 p3) =>
+            function(item, p1, p2, p3);
 
-        public static void Do<TItem>(this TItem item, Action<TItem> action) => action(item);
+        public static void Do<TItem>(this TItem item, Action<TItem> action) =>
+            action(item);
 
         public static void ForEach<TItem>(this IEnumerable<TItem> elements, Action<TItem> action)
         {
@@ -70,17 +77,23 @@ namespace AChildsCourage
             foreach (var element in elements) _ = function(element);
         }
 
-        public static TReplace ThenTake<TItem, TReplace>(this TItem _, TReplace item) => item;
+        public static TReplace ThenTake<TItem, TReplace>(this TItem _, TReplace item) =>
+            item;
 
-        public static TReturn FinallyReturn<TItem, TReturn>(this TItem _, TReturn item) => _.ThenTake(item);
+        public static TReturn FinallyReturn<TItem, TReturn>(this TItem _, TReturn item) =>
+            _.ThenTake(item);
 
-        public static bool Negate(this bool b) => !b;
+        public static bool Negate(this bool b) =>
+            !b;
 
-        public static TResult? Bind<TItem, TResult>(this TItem? item, Func<TItem, TResult> function) where TItem : struct where TResult : struct => item.HasValue ? function(item.Value) : (TResult?) null;
+        public static TResult? Bind<TItem, TResult>(this TItem? item, Func<TItem, TResult> function) where TItem : struct where TResult : struct =>
+            item.HasValue ? function(item.Value) : (TResult?) null;
 
-        public static TResult NullBind<TItem, TResult>(this TItem item, Func<TItem, TResult> function) where TItem : class where TResult : class => item != null ? function(item) : null;
+        public static TResult NullBind<TItem, TResult>(this TItem item, Func<TItem, TResult> function) where TItem : class where TResult : class =>
+            item != null ? function(item) : null;
 
-        public static TItem IfNull<TItem>(this TItem? item, TItem replacement) where TItem : struct => item ?? replacement;
+        public static TItem IfNull<TItem>(this TItem? item, TItem replacement) where TItem : struct =>
+            item ?? replacement;
 
         public static TAccumulate AggregateI<TSource, TAccumulate>(this IEnumerable<TSource> source, TAccumulate seed, Func<int, TAccumulate, TSource, TAccumulate> func)
         {
