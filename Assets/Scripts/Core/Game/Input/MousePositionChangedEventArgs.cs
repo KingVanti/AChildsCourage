1328177d07e147b8452f5901daefb47f9f4680f1
KingVanti <kingvanti650@gmail.com>
2020-12-30 10:28:59 +0100

    Reformat file

--- Assets/Scripts/Core/Game/Input/MousePositionChangedEventArgs.cs
+++ Assets/Scripts/Core/Game/Input/MousePositionChangedEventArgs.cs
@@ -7,17 +7,11 @@ namespace AChildsCourage.Game.Input
     internal class MousePositionChangedEventArgs : EventArgs
     {
 
-        #region Properties
-
         internal Vector2 MousePosition { get; }
 
-        #endregion
 
-        #region Constructors
-
-        internal MousePositionChangedEventArgs(Vector2 mousePosition) => MousePosition = mousePosition;
-
-        #endregion
+        internal MousePositionChangedEventArgs(Vector2 mousePosition) => 
+            MousePosition = mousePosition;
 
     }
 
