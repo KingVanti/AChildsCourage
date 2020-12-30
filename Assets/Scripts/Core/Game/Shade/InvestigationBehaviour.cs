fdb6319ea6d098d241341eaf9f23c13c4a5e62a4
KingVanti <kingvanti650@gmail.com>
2020-12-30 10:31:53 +0100

    Reformat file

--- Assets/Scripts/Core/Game/Shade/InvestigationBehaviour.cs
+++ Assets/Scripts/Core/Game/Shade/InvestigationBehaviour.cs
@@ -13,31 +13,32 @@ namespace AChildsCourage.Game.Shade
 
         private readonly CreateRng rng = RandomRng();
 
+        
         private Investigation currentInvestigation;
 
 
         public TilePosition CurrentTargetTile { get; private set; }
 
         public bool InvestigationIsInProgress => !IsComplete(currentInvestigation);
-
-
+        
+        
         public void StartNewInvestigation(FloorState floorState, ShadeState shadeState)
         {
             currentInvestigation = StartNew(floorState, shadeState, rng);
             UpdateTargetTile(shadeState.Position);
         }
 
-        private void UpdateTargetTile(EntityPosition position) => CurrentTargetTile = NextTarget(currentInvestigation, position);
-
+        private void UpdateTargetTile(EntityPosition position) => 
+            CurrentTargetTile = NextTarget(currentInvestigation, position);
 
         public void ProgressInvestigation(ShadeState shadeState, IEnumerable<TilePosition> investigatedTiles)
         {
             currentInvestigation = Progress(currentInvestigation, investigatedTiles);
             UpdateTargetTile(shadeState.Position);
         }
-
-
-        public CompletedInvestigation CompleteInvestigation() => Complete(currentInvestigation);
+        
+        public CompletedInvestigation CompleteInvestigation() =>
+            Complete(currentInvestigation);
 
     }
 
