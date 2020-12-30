24a5a773ebb73698d219e39e0ad288991480ac67
KingVanti <kingvanti650@gmail.com>
2020-12-30 10:29:14 +0100

    Reformat file

--- Assets/Scripts/Core/Game/Input/MoveDirectionChangedEventArgs.cs
+++ Assets/Scripts/Core/Game/Input/MoveDirectionChangedEventArgs.cs
@@ -7,17 +7,11 @@ namespace AChildsCourage.Game.Input
     internal class MoveDirectionChangedEventArgs : EventArgs
     {
 
-        #region Properties
-
         internal Vector2 MoveDirection { get; }
 
-        #endregion
 
-        #region Constructors
-
-        internal MoveDirectionChangedEventArgs(Vector2 moveDirection) => MoveDirection = moveDirection;
-
-        #endregion
+        internal MoveDirectionChangedEventArgs(Vector2 moveDirection) =>
+            MoveDirection = moveDirection;
 
     }
 
