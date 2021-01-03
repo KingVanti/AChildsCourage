7e0f36f7a15ea46215e550b7012c78a4fd0a61a4
KingVanti <kingvanti650@gmail.com>
2021-01-03 08:19:47 +0100

    Refactor map functions to own file

---
+++ Assets/Scripts/Core/F.Map.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AChildsCourage
+{
+
+    public static partial class F
+    {
+
+        public static TResult Map<TItem, TResult>(this TItem item, Func<TItem, TResult> function) =>
+            function(item);
+
+
+        public static TResult Map<TItem, TResult, TP1>(this TItem item, Func<TItem, TP1, TResult> function, TP1 p1) =>
+            function(item, p1);
+
+        public static TResult Map<TItem, TResult, TP1, TP2>(this TItem item, Func<TItem, TP1, TP2, TResult> function, TP1 p1, TP2 p2) =>
+            function(item, p1, p2);
+
+        public static TResult Map<TItem, TResult, TP1, TP2, TP3>(this TItem item, Func<TItem, TP1, TP2, TP3, TResult> function, TP1 p1, TP2 p2, TP3 p3) =>
+            function(item, p1, p2, p3);
+
+    }
+
+}
--- Assets/Scripts/Core/F.cs
+++ Assets/Scripts/Core/F.cs
@@ -6,7 +6,7 @@ using System.Linq;
 namespace AChildsCourage
 {
 
-    public static class F
+    public static partial class F
     {
 
         public static TItem Take<TItem>(TItem input) =>
@@ -51,18 +51,6 @@ namespace AChildsCourage
         {
             while (predicate()) action();
         }
-
-        public static TResult Map<TItem, TResult>(this TItem item, Func<TItem, TResult> function) =>
-            function(item);
-
-        public static TResult Map<TItem, TResult, TP1>(this TItem item, Func<TItem, TP1, TResult> function, TP1 p1) =>
-            function(item, p1);
-
-        public static TResult Map<TItem, TResult, TP1, TP2>(this TItem item, Func<TItem, TP1, TP2, TResult> function, TP1 p1, TP2 p2) =>
-            function(item, p1, p2);
-
-        public static TResult Map<TItem, TResult, TP1, TP2, TP3>(this TItem item, Func<TItem, TP1, TP2, TP3, TResult> function, TP1 p1, TP2 p2, TP3 p3) =>
-            function(item, p1, p2, p3);
 
         public static void Do<TItem, TP1>(this TItem item, Action<TItem, TP1> action, TP1 p1) =>
             action(item, p1);
