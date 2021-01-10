4fa999b77ffe2269df9b7212ebf8a15f06464639
KingVanti <kingvanti650@gmail.com>
2021-01-10 09:12:16 +0100

    Fix incorrect metod name

--- Assets/Scripts/Core/Game/Floors/Recreating/RuneSpawnerEntity.cs
+++ Assets/Scripts/Core/Game/Floors/Recreating/RuneSpawnerEntity.cs
@@ -13,9 +13,9 @@ namespace AChildsCourage.Game.Floors
 
 
         public void Spawn(TilePosition position, RuneData _) =>
-            InstantiateStaticObjectAt(position);
+            InstantiateRuneAt(position);
 
-        private void InstantiateStaticObjectAt(TilePosition tilePosition)
+        private void InstantiateRuneAt(TilePosition tilePosition)
         {
             var position = ToVector2(tilePosition) + new Vector2(0.5f, 0);
             Infrastructure.Spawn(runePrefab, position, Quaternion.identity, transform);
