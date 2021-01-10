1f2714ff4887104cef308eb17e29da7560ff05a1
KingVanti <kingvanti650@gmail.com>
2021-01-10 08:21:07 +0100

    Refactor for encapsulation

--- Assets/Scripts/Core/CustomMath.cs
+++ Assets/Scripts/Core/CustomMath.cs
@@ -4,65 +4,65 @@ using static UnityEngine.Mathf;
 namespace AChildsCourage
 {
 
-    public static class CustomMath
+    internal static class CustomMath
     {
 
-        public static float Map(float f, float sA, float sB, float tA, float tB) =>
+        internal static float Map(float f, float sA, float sB, float tA, float tB) =>
             Lerp(tA, tB, InverseLerp(sA, sB, f));
 
-        public static float Remap(this float f, float sA, float sB, float tA, float tB) =>
+        internal static float Remap(this float f, float sA, float sB, float tA, float tB) =>
             Map(f, sA, sB, tA, tB);
 
-        public static float Remap(this int i, int sA, int sB, float tA, float tB) =>
+        internal static float Remap(this int i, int sA, int sB, float tA, float tB) =>
             Map(i, sA, sB, tA, tB);
 
-        public static float RemapSquared(this float f, float sA, float sB, float tA, float tB) =>
+        internal static float RemapSquared(this float f, float sA, float sB, float tA, float tB) =>
             Lerp(tA, tB, InverseLerp(sA, sB, f).Squared());
 
-        public static float CalculateAngle(Vector2 vector) =>
+        internal static float CalculateAngle(Vector2 vector) =>
             CalculateAngle(vector.x, vector.y);
 
-        public static float CalculateAngle(float xPos, float yPos) =>
+        internal static float CalculateAngle(float xPos, float yPos) =>
             Atan2(yPos, xPos) * Rad2Deg;
 
-        public static float Clamp(this float f, float min, float max) =>
+        internal static float Clamp(this float f, float min, float max) =>
             f <= min ? min
             : f >= max ? max
             : f;
 
-        public static int Clamp(this int i, int min, int max) =>
+        internal static int Clamp(this int i, int min, int max) =>
             i <= min ? min
             : i >= max ? max
             : i;
 
-        public static float Inverse(this float f) =>
+        internal static float Inverse(this float f) =>
             1f / f;
 
-        public static float Raise(this float f, float pow) =>
+        internal static float Raise(this float f, float pow) =>
             Pow(f, pow);
 
-        public static float Squared(this float f) =>
+        internal static float Squared(this float f) =>
             Pow(f, 2);
 
-        public static int Minus(this int i, int sub) =>
+        internal static int Minus(this int i, int sub) =>
             i - sub;
 
-        public static float Plus(this float f, float add) =>
+        internal static float Plus(this float f, float add) =>
             f + add;
 
-        public static int Times(this int i, int mult) =>
+        internal static int Times(this int i, int mult) =>
             i * mult;
 
-        public static float Times(this int i, float mult) =>
+        internal static float Times(this int i, float mult) =>
             i * mult;
 
-        public static float CalculateCircleArea(float radius) =>
+        internal static float CalculateCircleArea(float radius) =>
             PI * radius.Squared();
 
-        public static float Mod(float mod, float f) =>
+        internal static float Mod(float mod, float f) =>
             f % mod;
 
-        public static float NormalizeAngle(float angle) =>
+        internal static float NormalizeAngle(float angle) =>
             Repeat(angle, 360);
 
     }
