42ae36e2b33441a3d85b65877b85953f5067b081
KingVanti <kingvanti650@gmail.com>
2021-01-13 08:12:17 +0100

    Ad logic for rune lighting up

--- Assets/Scripts/Core/Game/Floors/RuneEntity.cs
+++ Assets/Scripts/Core/Game/Floors/RuneEntity.cs
@@ -16,6 +16,8 @@ namespace AChildsCourage.Game.Floors
 
         [FindInScene] private FlashlightEntity flashLight;
 
+        [FindComponent(ComponentFindMode.OnChildren)]
+        private new RuneLight light;
         private RuneCharge charge = NoCharge;
 
 
@@ -25,18 +27,27 @@ namespace AChildsCourage.Game.Floors
 
         private float DistanceToFlashlight => Vector2.Distance(Center, FlashlightCenter);
 
-        private bool IsShoneOn => DistanceToFlashlight <= Radius + flashLight.ShineRadius;
+        private bool IsShoneOn => flashLight.IsTurnedOn && DistanceToFlashlight <= Radius + flashLight.ShineRadius;
 
         private float CurrentChargeGain => chargeGainRange.Map(Range.Lerp, flashLight.ShineDistanceInterpolation);
 
         private float CurrentChargeDelta => IsShoneOn ? CurrentChargeGain : -chargeDrain;
 
+        private RuneCharge Charge
+        {
+            get => charge;
+            set
+            {
+                charge = value;
+                light.UpdateLight(charge);
+            }
+        }
 
         private void Update() =>
             UpdateCharge();
 
-        private void UpdateCharge() => 
-            charge = charge.Map(Change, CurrentChargeDelta * Time.deltaTime);
+        private void UpdateCharge() =>
+            Charge = Charge.Map(Change, CurrentChargeDelta * Time.deltaTime);
 
     }
 
---
+++ Assets/Scripts/Core/Game/Floors/RuneLight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using static AChildsCourage.Range;
+
+namespace AChildsCourage.Game.Floors
+{
+
+    public class RuneLight : MonoBehaviour
+    {
+
+        [SerializeField] private Range<float> intensityRange;
+
+        [FindComponent] private new Light2D light;
+
+
+        public void UpdateLight(RuneCharge charge) =>
+            light.intensity = intensityRange.Map(Lerp, (float) charge);
+
+    }
+
+}
