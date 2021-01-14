53d4872becdb5012c08accf73c61b85a14c9dcd3
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2021-01-14 18:34:19 +0100

    Add stun when interacting with rift

---
+++ Assets/Scripts/Core/Game/Char/RiftEscapeEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AChildsCourage.Game.Char {
+    public class RiftEscapeEventArgs : EventArgs {
+
+        public bool IsEscapingThroughRift { get; }
+
+        public RiftEscapeEventArgs(bool isEscapingThroughRift) {
+            IsEscapingThroughRift = isEscapingThroughRift;
+        }
+
+    }
+
+
+}
