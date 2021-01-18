2cc37623f0a724e1981509b891e4c66e2448f5bc
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2021-01-18 15:48:15 +0100

    Revert "Refactor button sound effects"
    This reverts commit 98d47b1a3c5a77942d25a1162137c3fd3265d58d.

--- Assets/Scripts/Core/Menu/MainMenuEntity.cs
+++ Assets/Scripts/Core/Menu/MainMenuEntity.cs
@@ -17,10 +17,13 @@ namespace AChildsCourage.Menu.UI
         private static readonly int tutorialIndexKey = Animator.StringToHash("Tutorial");
         private static readonly int controlsIndexKey = Animator.StringToHash("Controls");
         private static readonly int creditsIndexKey = Animator.StringToHash("Credits");
-        private static readonly int noMenuIndexKey = Animator.StringToHash("NoMenu");
+
+        private EventInstance btn_ClickInstance;
+        private EventInstance btn_HoverInstance;
 
         public void OnPlayButtonPressed() => 
             Transition.To(SceneName.startCutscene, FadeColor.Black);
+
 
         public void OnQuitButtonPressed() =>
             Application.Quit();
@@ -31,8 +34,27 @@ namespace AChildsCourage.Menu.UI
 
         public void OnCreditsButtonPressed() => menuAnimationController.SetTrigger(creditsIndexKey);
 
-        public void OnDeselect() => menuAnimationController.SetTrigger(noMenuIndexKey);
+        public void btn_Hover()
+        {
+            btn_HoverInstance = RuntimeManager.CreateInstance(btnOnHover);
+            //btn_HoverInstance.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject));
+            btn_HoverInstance.start();
+            btn_HoverInstance.release();
+        }
+        public void btn_Click()
+        {
+            btn_ClickInstance = RuntimeManager.CreateInstance(btnOnClick);
+            //btn_ClickInstance.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject));
+            btn_ClickInstance.start();
+            btn_ClickInstance.release();
+        }
 
+        #region eventpaths
+        private const string btnOnHover = "event:/UI/Buttons/btn_hover";
+        private const string btnOnClick = "event:/UI/Buttons/btn_click";
+
+
+        #endregion
     }
 
 }
