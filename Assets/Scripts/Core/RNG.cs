c24f388ccf8e53c6c1da53ac4c8be71950c59a11
KingVanti <kingvanti650@gmail.com>
2021-01-19 08:17:34 +0100

    Refactor for readability

--- Assets/Scripts/Core/RNG.cs
+++ Assets/Scripts/Core/RNG.cs
@@ -10,40 +10,38 @@ namespace AChildsCourage
         private const float HundredPercent = 100f;
         
 
-        public static Rng RngFromSeed(int seed)
+        internal static Rng RngFromSeed(int seed)
         {
             var random = new Random(seed);
 
             return new Rng(() => (float) random.NextDouble());
         }
 
-        public static Rng ConstantRng(float value) => new Rng(() => value);
-
+        internal static Rng ConstantRng(float value) => new Rng(() => value);
         
-        public static Rng RandomRng() =>
+        internal static Rng RandomRng() =>
             RngFromSeed(DateTime.Now.GetHashCode());
         
         private static float NextValue(Rng rng) =>
             rng.source();
         
-        public static float GetValueBetween(float min, float max, Rng rng)
+        internal static float GetValueBetween(float min, float max, Rng rng)
         {
             var diff = max - min;
             var dist = diff * rng.Map(NextValue);
-
             return min + dist;
         }
         
-        public static int GetValueBetween(int min, int max, Rng rng) =>
+        internal static int GetValueBetween(int min, int max, Rng rng) =>
             (int) rng.Map(GetValueBetween, (float) min, (float) max);
         
-        public static float GetValueUnder(float max, Rng rng) =>
+        internal static float GetValueUnder(float max, Rng rng) =>
             rng.Map(NextValue) * max;
         
-        public static int GetValueUnder(int max, Rng rng) =>
+        internal static int GetValueUnder(int max, Rng rng) =>
             (int) rng.Map(GetValueUnder, (float) max);
         
-        public static bool Prob(float variantProb, Rng rng) =>
+        internal static bool Prob(float variantProb, Rng rng) =>
             rng.Map(GetValueBetween, MinPercent, HundredPercent) <= variantProb;
 
 
