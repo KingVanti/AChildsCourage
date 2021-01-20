2a883a6a631531d06439cf90ec173a3ff4278397
KingVanti <kingvanti650@gmail.com>
2021-01-20 08:26:20 +0100

    Drop unused code

--- Assets/Scripts/Core/Game/TilePositionCollection.cs
+++ Assets/Scripts/Core/Game/TilePositionCollection.cs
@@ -1,25 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
 
 namespace AChildsCourage.Game
 {
 
     public readonly struct TilePositionCollection : IEnumerable<TilePosition>
     {
-        
-        public static TilePosition Average(TilePositionCollection positions) =>
-            new TilePosition((int) positions.Select(p => p.X).Average(),
-                             (int) positions.Select(p => p.Y).Average());
-        
 
         private readonly ImmutableHashSet<TilePosition> tilePositions;
 
 
-        public TilePositionCollection(ImmutableHashSet<TilePosition> tilePositions) =>
-            this.tilePositions = tilePositions;
-        
         public TilePositionCollection(IEnumerable<TilePosition> tilePositions) =>
             this.tilePositions = tilePositions.ToImmutableHashSet();
 
