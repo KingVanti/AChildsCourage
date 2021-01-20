80f8c39ea2fce678557136563e4695ff4c4181bb
KingVanti <kingvanti650@gmail.com>
2021-01-20 13:35:10 +0100

    Fix incorrct collider offsets

--- Assets/Scripts/Core/Game/Floors/StaticObjectEntity.cs
+++ Assets/Scripts/Core/Game/Floors/StaticObjectEntity.cs
@@ -13,18 +13,41 @@ namespace AChildsCourage.Game.Floors
         internal Sprite Sprite
         {
             get => spriteRenderer.sprite;
-            set {
+            set
+            {
                 spriteRenderer.sprite = value;
                 FitColliderToSprite();
             }
         }
 
-        private void FitColliderToSprite() {
-            coll.size = 
-                new Vector2(Sprite.bounds.size.x - (Sprite.border.x + Sprite.border.z) / Sprite.pixelsPerUnit,
-                Sprite.bounds.size.y - (Sprite.border.w + Sprite.border.y) / Sprite.pixelsPerUnit);
-            coll.offset = new Vector2(0, coll.bounds.extents.y + Sprite.border.y/Sprite.pixelsPerUnit);
+        private void FitColliderToSprite()
+        {
+            coll.size = CalculateBoxSize();
+            coll.offset = CalculateBoxOffset();
         }
+
+        private Vector2 CalculateBoxOffset() => 
+            CalculateBoxCenter() - new Vector2(0.5f, 0);
+
+        private Vector2 CalculateBoxCenter()
+        {
+            var borders = CalculateWorldSpaceBorders();
+            var size = CalculateBoxSize();
+
+            return new Vector2(borders.x + size.x / 2,
+                               borders.y + size.y / 2);
+        }
+
+        private Vector2 CalculateBoxSize()
+        {
+            var borders = CalculateWorldSpaceBorders();
+
+            return new Vector2(Sprite.bounds.size.x - (borders.x + borders.z),
+                               Sprite.bounds.size.y - (borders.y + borders.w));
+        }
+
+        private Vector4 CalculateWorldSpaceBorders() =>
+            Sprite.border / Sprite.pixelsPerUnit;
 
     }
 
