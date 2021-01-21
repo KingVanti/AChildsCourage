58e67ce155a2f1c4bc2e6567d8aae311c65cd7cc
KingVanti <kingvanti650@gmail.com>
2021-01-21 07:41:34 +0100

    Reformat file

--- Assets/Scripts/Core/Cutscene/FmodCutscenes.cs
+++ Assets/Scripts/Core/Cutscene/FmodCutscenes.cs
@@ -1,13 +1,11 @@
 ﻿using FMODUnity;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FmodCutscenes : MonoBehaviour
 {
+
     public void startAudio() => RuntimeManager.PlayOneShot("event:/cutScenes/startAudio");
 
     public void endAudio() => RuntimeManager.PlayOneShot("event:/cutScenes/endAudio");
-
 
 }
